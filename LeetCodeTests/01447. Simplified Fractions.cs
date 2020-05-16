bb7e31c69568cffa614c0fac54b34bb7bf36cdcf
kirtsis <kirtsis@gmail.com>
2020-05-16 20:13:16 +0300

    1447. Simplified Fractions

---
+++ LeetCodeTests/01447. Simplified Fractions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace LeetCodeTests {
+
+    /// <summary>
+    ///     1447. Simplified Fractions
+    ///     https://leetcode.com/problems/simplified-fractions/
+    /// </summary>
+    [TestFixture]
+    public class P01447 {
+
+        [PublicAPI]
+        public IList<String> SimplifiedFractions(Int32 n) {
+            // Given an integer n, return a list of all simplified fractions between 0 and 1 (exclusive) such that the denominator is less-than-or-equal-to n. The fractions can be in any order.
+            //
+            // Constraints:
+            // * 1 <= n <= 100
+
+            var result = new List<String>();
+
+            var set = new HashSet<Decimal>();
+            for (Decimal numerator = 1; numerator <= n; ++numerator) {
+                for (Decimal denominator = 1; denominator <= n; ++denominator) {
+                    Decimal fraction = numerator / denominator;
+                    if (fraction >= 1) continue;
+                    if (!set.Add(numerator / denominator)) continue;
+
+                    result.Add($"{numerator}/{denominator}");
+                }
+            }
+
+            return result;
+        }
+
+        [Test]
+        [TestCase(1, ExpectedResult = "[]")]
+        [TestCase(2, ExpectedResult = "[\"1/2\"]")]
+        [TestCase(3, ExpectedResult = "[\"1/2\",\"1/3\",\"2/3\"]")]
+        [TestCase(4, ExpectedResult = "[\"1/2\",\"1/3\",\"1/4\",\"2/3\",\"3/4\"]")]
+        [TestCase(20, ExpectedResult = "[\"1/2\",\"1/3\",\"1/4\",\"1/5\",\"1/6\",\"1/7\",\"1/8\",\"1/9\",\"1/10\",\"1/11\",\"1/12\",\"1/13\",\"1/14\",\"1/15\",\"1/16\",\"1/17\",\"1/18\",\"1/19\",\"1/20\",\"2/3\",\"2/5\",\"2/7\",\"2/9\",\"2/11\",\"2/13\",\"2/15\",\"2/17\",\"2/19\",\"3/4\",\"3/5\",\"3/7\",\"3/8\",\"3/10\",\"3/11\",\"3/13\",\"3/14\",\"3/16\",\"3/17\",\"3/19\",\"3/20\",\"4/5\",\"4/7\",\"4/9\",\"4/11\",\"4/13\",\"4/15\",\"4/17\",\"4/19\",\"5/6\",\"5/7\",\"5/8\",\"5/9\",\"5/11\",\"5/12\",\"5/13\",\"5/14\",\"5/16\",\"5/17\",\"5/18\",\"5/19\",\"6/7\",\"6/11\",\"6/13\",\"6/17\",\"6/19\",\"7/8\",\"7/9\",\"7/10\",\"7/11\",\"7/12\",\"7/13\",\"7/15\",\"7/16\",\"7/17\",\"7/18\",\"7/19\",\"7/20\",\"8/9\",\"8/11\",\"8/13\",\"8/15\",\"8/17\",\"8/19\",\"9/10\",\"9/11\",\"9/13\",\"9/14\",\"9/16\",\"9/17\",\"9/19\",\"9/20\",\"10/11\",\"10/13\",\"10/17\",\"10/19\",\"11/12\",\"11/13\",\"11/14\",\"11/15\",\"11/16\",\"11/17\",\"11/18\",\"11/19\",\"11/20\",\"12/13\",\"12/17\",\"12/19\",\"13/14\",\"13/15\",\"13/16\",\"13/17\",\"13/18\",\"13/19\",\"13/20\",\"14/15\",\"14/17\",\"14/19\",\"15/16\",\"15/17\",\"15/19\",\"16/17\",\"16/19\",\"17/18\",\"17/19\",\"17/20\",\"18/19\",\"19/20\"]")]
+        public String Test(Int32 n) {
+            IList<String> result = this.SimplifiedFractions(n);
+            return JsonConvert.SerializeObject(result);
+        }
+
+    }
+
+}
